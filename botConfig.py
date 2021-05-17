8d4fe7f9f11d3b81cc668523a48ae75ab61c29a6
marianodominguez <mariano.dm@gmail.com>
2021-05-16 17:24:34 -0700

    use nonpriv user

--- botConfig.py
+++ botConfig.py
@@ -5,10 +5,10 @@ import logging
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = "???"
-    consumer_secret = "???"
-    access_token = "???"
-    access_token_secret = "???"
+    consumer_key = "tMA7JsNnVs95BU4nTmvXlLptG"
+    consumer_secret = "RFBAYG9iWoWf0IHCIxBXBFvisvBLjH8dQOqRratksuLauIsyk7"
+    access_token = "1392626957536284674-3ppfPG5XxXWl00vBtsIAf2Pi58w9JW"
+    access_token_secret = "XqOM36dzD4E6qKjOrJJJz0A4bENv22SB4RhBoMHvqcWk2"
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
