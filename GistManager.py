f9da04e53b043c7ebef2971a09f6cd0995014541
marianodominguez <mariano.dm@gmail.com>
2021-06-19 23:38:57 -0700

    add gist manager

--- GistManager.py
+++ GistManager.py
@@ -1,5 +1,14 @@
-import url2
+import urllib2
+import sys
 
-def getGist(url):
-    if not "https://gist.githubusercontent.com" in url:
+def getGist(gistUrl):
+    if not "https://gist.githubusercontent.com" in gistUrl:
         return "Error: Not a valid URL"
+    response = urllib2.urlopen(gistUrl)
+    data = response.read()
+    if len(data) > 2000:
+        return "Error, only 2k of code is allowed"
+    return data
+    
+if __name__ == "__main__":
+    print getGist(sys.argv[1])
