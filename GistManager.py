b86ee976b9687d5960d7b478985bb96420e03f03
DrMD <mariano.dm@gmail.com>
2021-06-21 18:36:27 +0000

    add unicode test

--- GistManager.py
+++ GistManager.py
@@ -2,6 +2,7 @@ import urllib.request as urllib2
 import sys
 import urllib
 from unidecode import unidecode
+import re
 
 def getGist(gistUrl):
     if not "https://gist.githubusercontent.com" in gistUrl:
@@ -15,3 +16,8 @@ def getGist(gistUrl):
         return "Error: only 2k of code is allowed"
     return data.decode('utf8')
 
+if __name__ == "__main__":
+    result = getGist('https://gist.githubusercontent.com/marianodominguez/b4e4cf03b821d4e48bea064c4fa73574/raw/da7dd97991cec4f7aaa3496dcf2b09693c0b749d/moire2.bas')
+    exp = "{\w*(?:}\s*)" #{anything till } plus trailing whitespace
+    basicode = re.sub(exp,'',result)
+    print(basicode)
