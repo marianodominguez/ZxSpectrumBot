781eddabea09c47d31b5523b112cce613743a03a
Mariano Dominguez <marianod@wepay.com>
2021-06-22 19:46:29 -0700

    add emulator

---
+++ Emulator.py
@@ -0,0 +1,38 @@
+import tweepy
+import logging
+import time
+import os,sys
+import subprocess
+
+def run_emulator(logger, api, language, recordtime, starttime):
+    logger.info("Firing up emulator")
+    if language==0 or language==2: #BASIC or ASM
+        cmd = '/usr/bin/fuse-sdl --fbmode 640 --graphics-filter 2x --no-confirm-actions --no-autosave-settings --auto-load --no-sound --tape working/tape.tap'.split()
+
+    emuPid = subprocess.Popen(cmd, env={"DISPLAY": ":99","SDL_AUDIODRIVER": "dummy"})
+    logger.info(f"   Process ID {emuPid.pid}")
+
+    time.sleep(starttime)
+
+    logger.info("Recording with ffmpeg")
+    result = os.system(f'ffmpeg -y -hide_banner -loglevel warning -f x11grab -r 30 -video_size 672x440 -i :99 -q:v 0 -pix_fmt yuv422p -t {recordtime} working/OUTPUT_BIG.mp4')
+
+    logger.info("Stopping emulator")
+    emuPid.kill()
+
+    logger.info("Converting video")
+    result = os.system('ffmpeg -loglevel warning -y -i working/OUTPUT_BIG.mp4 -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p -strict experimental -r 30 -t 2:20 -acodec aac -vb 1024k -minrate 1024k -maxrate 1024k -bufsize 1024k -ar 44100 -ac 2 working/OUTPUT_SMALL.mp4')
+    #per https://gist.github.com/nikhan/26ddd9c4e99bbf209dd7#gistcomment-3232972
+
+    logger.info("Uploading video")  
+
+    media = api.media_upload("working/OUTPUT_SMALL.mp4", chunked=True)
+
+    logger.info(f"Media ID is {media.media_id}")
+
+    time.sleep(5)
+    #TODO replace with get_media_upload_status per https://github.com/tweepy/tweepy/pull/1414
+
+    logger.info(f"Posting tweet to @{tweet.user.screen_name}")
+    tweettext = f"@{tweet.user.screen_name} "
+    api.update_status(auto_populate_reply_metadata=False, status=tweettext, media_ids=[media.media_id], in_reply_to_status_id=tweet.id)
--- SpeccyBot.py
+++ SpeccyBot.py
@@ -4,9 +4,9 @@ import tweepy
 import logging
 from botConfig import create_api
 import time
+import os
 from shutil import copyfile
-import os,sys
-import subprocess
+import Emulator
 from datetime import datetime
 from unidecode import unidecode
 import re
@@ -18,12 +18,9 @@ logger = logging.getLogger()
 def determine_config(full_text, gistUrl):
     config={}
     config['error']=None
-    
     #remove all @ mentions, leaving just the BASIC code
     basiccode = re.sub('^(@.+?\s)+','',full_text, re.ASCII)
-
     basiccode = unidecode(basiccode)
-
     #unescape >, <, and &
     basiccode = basiccode.replace("&lt;", "<")
     basiccode = basiccode.replace("&gt;", ">")
@@ -37,10 +34,7 @@ def determine_config(full_text, gistUrl):
             config['error']=text
             return config
         else:
-            basicode = unidecode(text) 
-
-    #determine language:
-
+            basicode = unidecode(text)
     #look for Debug command
     exp = "{\w*?D\w*(?:}|\s)" # {B\d\d  D= debug
     result = re.search(exp,basiccode)
@@ -160,38 +154,7 @@ def check_mentions(api, since_id):
             if debug:
                 reply_tweet(api, tweet, result[:280])
             continue
-
-        logger.info("Firing up emulator")
-        if language==0 or language==2: #BASIC or ASM
-            cmd = '/usr/bin/fuse-sdl --fbmode 640 --graphics-filter 2x --no-confirm-actions --no-autosave-settings --auto-load --no-sound --tape working/tape.tap'.split()
-
-        emuPid = subprocess.Popen(cmd, env={"DISPLAY": ":99","SDL_AUDIODRIVER": "dummy"})
-        logger.info(f"   Process ID {emuPid.pid}")
-
-        time.sleep(starttime)
-
-        logger.info("Recording with ffmpeg")
-        result = os.system(f'ffmpeg -y -hide_banner -loglevel warning -f x11grab -r 30 -video_size 672x440 -i :99 -q:v 0 -pix_fmt yuv422p -t {recordtime} working/OUTPUT_BIG.mp4')
-
-        logger.info("Stopping emulator")
-        emuPid.kill()
-
-        logger.info("Converting video")
-        result = os.system('ffmpeg -loglevel warning -y -i working/OUTPUT_BIG.mp4 -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p -strict experimental -r 30 -t 2:20 -acodec aac -vb 1024k -minrate 1024k -maxrate 1024k -bufsize 1024k -ar 44100 -ac 2 working/OUTPUT_SMALL.mp4')
-        #per https://gist.github.com/nikhan/26ddd9c4e99bbf209dd7#gistcomment-3232972
-
-        logger.info("Uploading video")  
-
-        media = api.media_upload("working/OUTPUT_SMALL.mp4", chunked=True)
-
-        logger.info(f"Media ID is {media.media_id}")
-
-        time.sleep(5)
-        #TODO replace with get_media_upload_status per https://github.com/tweepy/tweepy/pull/1414
-
-        logger.info(f"Posting tweet to @{tweet.user.screen_name}")
-        tweettext = f"@{tweet.user.screen_name} "
-        api.update_status(auto_populate_reply_metadata=False, status=tweettext, media_ids=[media.media_id], in_reply_to_status_id=tweet.id)
+        Emulator.run_emulator(logger, api, language, recordtime, starttime)
 
         logger.info("Done!")
     return new_since_id
