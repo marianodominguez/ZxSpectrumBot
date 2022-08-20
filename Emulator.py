ce918819c904c9564286fd9cc8135763e74e4dce
Mariano Dominguez <mariano.dm@gmail.com>
2022-08-19 23:28:37 -0700

    use video

--- Emulator.py
+++ Emulator.py
@@ -109,7 +109,7 @@ def run_emulator(api, tweet, config):
 
     logger.info("Uploading video")  
 
-    media = api.media_upload("working/OUTPUT_SMALL.mp4", chunked=True, media_category='amplify')
+    media = api.media_upload("working/OUTPUT_SMALL.mp4", chunked=True, media_category='tweet_video')
 
     logger.info(f"Media ID is {media.media_id}")
 
