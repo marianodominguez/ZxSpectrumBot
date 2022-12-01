36c43e13ab4f0a28583e2d8b5bae1c8029ba99df
Mariano Dominguez <mariano.dm@gmail.com>
2022-11-30 20:30:06 -0800

    remove compression in capture, due to bug with xvfb add default rc

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 #avoid to get all twitters, update this to a real ID
 #echo '1568012337696874497' > /home/zxspectrum/bot/sinceFile.txt
 
-echo '109424210463485644' > /home/zxspectrum/bot/sinceFile.txt
+touch  /home/zxspectrum/.fuserc
+echo '109429915290829699' > /home/zxspectrum/bot/sinceFile.txt
 nohup Xvfb :99 > /dev/null 2>&1 &
 python3 SpeccyBot.py
